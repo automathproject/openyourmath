80d153b004bcb2a09ff7c3ab70b97332ffc27b0e
smaxx73 <ngmaxime@gmail.com>
2024-10-16 08:57:15 +0200

    modif header

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -29,7 +29,7 @@
 	</nav>
 
 	<div class="corner">
-		<a href="https://github.com/sveltejs/kit">
+		<a href="https://github.com/automathproject/openyourmath/tree/main/src">
 			<img src={github} alt="GitHub" />
 		</a>
 	</div>
