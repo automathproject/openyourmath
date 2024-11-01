dd1de4188d9fc8158f6f6c1ffa2ee0ec0def9487
smaxx73 <ngmaxime@gmail.com>
2024-11-01 15:35:29 +0100

    suppression about

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -25,9 +25,6 @@
 				<a href="/exercice">Exercices</a>
 				<li aria-current={$page.url.pathname === '/exercice/liste' ? 'page' : undefined}>
 					<a href="/exercice/liste">Liste</a>
-			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about">About</a>
-			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
