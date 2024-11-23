b3142e855cc34f777ae88259c8ce15b6d8a8d7aa
smaxx73 <ngmaxime@gmail.com>
2024-11-23 14:52:53 +0100

    factorisation bouttons

--- src/components/ExerciceRenderer.svelte
+++ src/components/ExerciceRenderer.svelte
@@ -65,14 +65,7 @@ function toggleMetadata() {
     processedContenu = [];
   }
 
-  function formatDate(dateString: string): string {
-    const date = new Date(dateString);
-    return date.toLocaleDateString("fr-FR", {
-      day: "2-digit",
-      month: "long",
-      year: "numeric",
-    });
-  }
+
 </script>
 
 <div
